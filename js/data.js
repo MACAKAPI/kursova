321bca651aa0b357bbbafd173f49fcbb49f40bbf
MACAKAPI <78927443+MACAKAPI@users.noreply.github.com>
2022-05-10 20:28:27 +0300

    update_1

---
+++ js/data.js
@@ -0,0 +1,67 @@
+const dataCountry=[
+    {country:   "Німеччина",
+    population: 84279000,
+    infections: 25385707,
+    deceased:   137020,
+    recovered:  23536900},
+
+    {country:   "Білорусь",
+    population: 9444000,
+    infections: 982809,
+    deceased:   6974,
+    recovered:  928536},
+
+    {country:   "Естонія",
+    population: 1328000,
+    infections: 573922,
+    deceased:   2556,
+    recovered:  514836},
+
+    {country:   "Латвія",
+    population: 1848000,
+    infections: 824635,
+    deceased:   5790,
+    recovered:  814141},
+
+    {country:   "Литва",
+    population: 2652000 ,
+    infections: 1059836,
+    deceased:   9121,
+    recovered:  1030233},
+
+    {country:   "Польща",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0},
+
+    {country:   "Сербія",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0},
+
+    {country:   "Хорватія",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0},
+
+    {country:   "Словаччина",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0},
+
+    {country:   "Угорщина",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0},
+
+    {country:   "Україна",
+    population: 0,
+    infections: 0,
+    deceased:   0,
+    recovered:  0}
+]
